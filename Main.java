679ef4938d28774c28a30c1f1a902f646b45dbf9
KakyungLee <eeww95@gmail.com>
2017-07-08 10:56:18 +0900

    p_7576

--- Main.java
+++ Main.java
@@ -1,119 +1,135 @@
-package p_2667;
+package p_7576;
 
 import java.util.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
-		int row;
+		int row, col;
 		Scanner scan = new Scanner(System.in);
-		String temp = new String();
-		
-		//마을 만들기
-		row = scan.nextInt();scan.nextLine();
-		int[][] town = new  int[row][row];
-		
-		for(int i=0; i<row; i++)
-		{
-			
-			temp = scan.nextLine();
-			
-			for(int j=0; j<row; j++)
-			{
-				town[i][j] = temp.charAt(j)-'0';
-			}
-		}
-		
-		ArrayList<Integer> arr = new ArrayList<Integer>();
-		int count = 1;
-		for(int i=0; i<row; i++)
-		{
-			for(int j=0; j<row; j++)
-			{
-				if(town[i][j] == 1)
-				{
-					count = 1;
-					Queue<Vertex> que = new LinkedList<Vertex>();
-					que.offer(new Vertex(i,j));
-					town[i][j] = 2;
-					
-					while(!que.isEmpty())
-					{
-						Vertex tempV = que.poll();
-						
-						if(tempV.i>0)  //위
-						{
-							if(town[tempV.i-1][tempV.j] == 1)
-							{
-								que.offer(new Vertex(tempV.i-1,tempV.j));
-								town[tempV.i-1][tempV.j] = 2;
-								count++;
-							}
-						}
-						
-						if(tempV.j<row-1)  // 오른쪽
-						{
-							if(town[tempV.i][tempV.j+1] == 1)
-							{
-								que.offer(new Vertex(tempV.i,tempV.j+1));
-								town[tempV.i][tempV.j+1] = 2;
-								count++;
-							}
-						}
-						
-						if(tempV.i<row-1)  //아래
-						{
-							if(town[tempV.i+1][tempV.j] == 1)
-							{
-								que.offer(new Vertex(tempV.i+1,tempV.j));
-								town[tempV.i+1][tempV.j] = 2;
-								count++;
-							}
-						}
-						
-						if(tempV.j>0)  //왼쪽
-						{
-							if(town[tempV.i][tempV.j-1] == 1)
-							{
-								que.offer(new Vertex(tempV.i,tempV.j-1));
-								town[tempV.i][tempV.j-1] = 2;
-								count++;
-							}
-						}
-												
-					}
-					arr.add(count);
-				}	
-				
-			}
-		}
+		int rt=0,nt=0, ut=0; // 익은 토마토 , 토마토 없음, 익지않은 토마토
+		Queue<Vertex> queue = new LinkedList<Vertex>();
 		
 		
-		System.out.println(arr.size());
-		
-		Collections.sort(arr);
-		
-		for(int i=0; i<arr.size(); i++)
-			System.out.println(arr.get(i));
-		
+		// 토마트 박스에 넣기
+		row = scan.nextInt();
+		col = scan.nextInt();
 
+		int[][] box = new int[col][row];
+		
+		for(int i=0; i<col; i++)
+		{
+			for(int j=0; j<row; j++)
+			{
+				box[i][j] = scan.nextInt();
+				if(box[i][j] == 1)
+				{
+					rt++;
+					queue.offer(new Vertex(j,i));
+				}else if(box[i][j] == 0)
+				{
+					ut++;
+				}else {
+					nt++;
+				}
+			}
+		}
+		
+		
+		if(rt+nt == row*col) // 익은 토마토가 가득차있는 경우
+		{
+			System.out.println(0);
+			return;
+		}else if(nt+ut == row*col) // 익은 토마토가 하나도 없는 경우 & 토마토가 한개도 없는 경우
+		{
+			System.out.println(-1);
+			return;
+		}
+		
+		
+		//
+		int count = -1;
+		int check = rt;
+		int addRt = 0;
+		
+		while(!queue.isEmpty())
+		{
+			Vertex temp = queue.poll();	
+			check--;
+			
+			if(temp.visited == false)
+			{
+				if(temp.Ctemp > 0) // 위
+				{
+					if(box[temp.Ctemp-1][temp.Rtemp] == 0) {
+					rt++;
+					box[temp.Ctemp-1][temp.Rtemp] = 1;
+					queue.offer(new Vertex(temp.Rtemp,temp.Ctemp-1));
+					addRt++;
+					}
+				}
+				if(temp.Rtemp < row-1 ) // 오른쪽
+				{
+					if(box[temp.Ctemp][temp.Rtemp+1] == 0) {
+					rt++;
+					box[temp.Ctemp] [temp.Rtemp+1]= 1;
+					queue.offer(new Vertex(temp.Rtemp+1,temp.Ctemp));
+					addRt++;
+					}
+				}
+				if(temp.Ctemp < col-1 ) //아래
+				{
+					if(box[temp.Ctemp+1][temp.Rtemp] == 0)
+					{
+					rt++;
+					box[temp.Ctemp+1] [temp.Rtemp]= 1;
+					queue.offer(new Vertex(temp.Rtemp,temp.Ctemp+1));
+					addRt++;
+					}
+				}
+				if(temp.Rtemp > 0 ) // 왼쪽
+				{
+					if( box[temp.Ctemp][temp.Rtemp-1] == 0) 
+					{
+					rt++;
+					box[temp.Ctemp][temp.Rtemp-1] = 1;
+					queue.offer(new Vertex(temp.Rtemp-1,temp.Ctemp));
+					addRt++;
+					}
+				}				
+				
+				temp.visited = true;
+				
+				if(check == 0)
+				{
+					check = addRt;
+					addRt =0;
+					count++;
+				}
+			}			
+			
+		}
+		
+		if(rt + nt < row * col)
+		{
+			System.out.println(-1);
+			return;
+		}
+		
+		System.out.println(count);
 
 	}
 
 }
 
 class Vertex{
-	int i,j;
+	int Rtemp, Ctemp;
 	int data;
 	boolean visited = false;
 	
-	Vertex(int _i, int _j)
+	Vertex(int row, int col)
 	{
-		i = _i;
-		j = _j;
+		Rtemp = row;
+		Ctemp = col;
 	}
 }
-
-
-
